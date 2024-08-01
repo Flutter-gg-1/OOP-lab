11264672a3ac8914f151583e4a3f6813effba395
Linda Mak <LindaAlmakramy@gmail.com>
2024-08-01 14:12:21 +0300

    edited main

---
+++ ooplab/bin/books.dart
@@ -0,0 +1,22 @@
+class Books{
+  //attributes
+  int? bookId;
+  String? booktitle;
+  String? auther;
+  String? type;
+
+  //Constructors
+  Books({required this.bookId, required this.booktitle,required this.auther, required this.type}){
+  }
+  //Functions
+  void addBook(){
+
+  }
+    void editBook(){
+
+  }
+
+    void edeleteBook(){
+
+  }
+}
--- ooplab/bin/ooplab.dart
+++ ooplab/bin/ooplab.dart
@@ -1,5 +1,27 @@
-import 'package:ooplab/ooplab.dart' as ooplab;
+
+import 'dart:io';
 
 void main(List<String> arguments) {
-  print('Hello world: ${ooplab.calculate()}!');
+  print("--------Welcome Admin--------");
+  print("What do you want to do?");
+  print("1-Edit books \n 2-Edit empolyees \n 3-Edit members \n 4-Edit events");
+  String? choice = stdin.readLineSync(); 
+  while(choice != 'e'){
+      switch (choice){
+        case '1':
+        print('somthing');
+        choice ='e';
+        case '2':
+        //do somthing
+        case '3':
+        //do somthing
+        case '4':
+        //do somthing
+        case 'e':
+        default:
+      print("Please try again");
+      choice = stdin.readLineSync();   }
+    }
+    print("--------GoodBye--------");
+    exit(0);
 }
