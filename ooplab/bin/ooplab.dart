bcc1ce682836b183845ce3d45ae7d1dc3250caa2
yara albouq <yaramalbouq@gmail.com>
2024-08-01 14:36:11 +0300

    Event class last edit

--- ooplab/bin/ooplab.dart
+++ ooplab/bin/ooplab.dart
@@ -1,14 +1,13 @@
 import 'dart:io';
 import 'dart:math';
 
+//Event class display events with it is name,id,date and supervisor name
 class Events {
-  //At
   //Attrabutes
   String? name = stdin.readLineSync();
-  String? id = "${Random().nextInt(999)}"; 
+  String? id = "${Random().nextInt(999)}";
   String? supervisor = stdin.readLineSync();
   String? date = stdin.readLineSync();
-  List<String> partispentes = [];
 
   //constactor
   Events({
@@ -16,30 +15,55 @@ class Events {
     required this.id,
     required this.supervisor,
     required this.date,
-    required this.partispentes,
   });
   List<Map<String, dynamic>> enventListMap = [{}];
 
   //functions
-  addEvent({required id}) {
-    List<Map<String, dynamic>> enventlist = [
-      {
-        "name": name,
-        "id": id,
-        "supervisor": supervisor,
-        "date": date,
-        "partispentes": partispentes,
-      }
-    ];
-    addEvent.add(enventlist);
-  print("* account add successfully *");
-  print("\n");
-  print("your account number is ***** $accountNumber *****");
+  //this function will add new event
+  addEvent() {
+    Map<String, dynamic> envent = {
+      "name": name,
+      "id": id,
+      "supervisor": supervisor,
+      "date": date,
+    };
+    enventListMap.add(envent);
 
-  stdin.readLineSync(); //for new line after display
-}
 
+    print("* Event add successfully *");
+    print("\n");
+    print("your ID is  $id ");
+
+    stdin.readLineSync(); //for new line after display
   }
 
+  //this function will display all event
+  displayEvent({required List<Map<String, dynamic>> eventDisplay}) {
+    for (var element in eventDisplay) {
+      print("                   All Events      ");
+      print(" Event number is ${element["name"]}");
+      print(" ID is ${element["id"]}");
+      print(" supervisor is ${element["supervisor"]}");
+      print(" Date of event is ${element["date"]}");
+    }
+    stdin.readLineSync(); //for new line after display
+  }
 
+//this function will remove event by event id
+  removeEvent({required String eventId}) {
+    Map<String, dynamic> event = {};
+    for (var element in enventListMap) {
+      if (element["id"] == eventId) {
+        event = element;
+        break;
+      }
+    }
+    //to ensure the number enterd founds
+    if (event.isNotEmpty) {
+      enventListMap.remove(event); // this function remove id
+    } else {
+      print("Event number $eventId not found! ");
+    }
+    stdin.readLineSync(); //for new line after display
+  }
 }
